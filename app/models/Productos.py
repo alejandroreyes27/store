4db6d779714ea3bc41e73716ceea2adecb82350b
alejandroreyes27 <alejandroariza.cr@gmail.com>
2025-05-05 13:35:00 -0500

    ADD campo de talla para los zapatos

--- app/models/Productos.py
+++ app/models/Productos.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 class Productos(db.Model):
     __tablename__ = 'productos'
-    
     idProducto = db.Column(db.Integer, primary_key=True)
     nombreProducto = db.Column(db.String(255), nullable=False)
     descripcionProducto = db.Column(db.Text, nullable=False)
@@ -11,7 +10,9 @@ class Productos(db.Model):
     stock = db.Column(db.Integer, nullable=False)
     imagenProducto = db.Column(db.String(255), nullable=True)
     
-    # Clave foránea para la relación con Categoria
+    # Nuevo campo talla
+    talla = db.Column(db.String(10), nullable=True)
+
     idCategoria = db.Column(db.Integer, db.ForeignKey('categoria.idCategoria'), nullable=True)
     categoria = db.relationship('Categoria', back_populates='productos')
     fechaCreacion = db.Column(db.DateTime, default=datetime.utcnow)
